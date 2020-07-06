a1eda4ee3f24eb64be9b24fa2a6c9ffabb8dcf26
JuanHenao24 <juan.1701811735@ucaldas.edu.co>
2020-07-05 21:14:25 -0500

    se crea una autorizacion de usuario(incompleta)

--- src/controllers/registro.controller.ts
+++ src/controllers/registro.controller.ts
@@ -1,173 +1,61 @@
+import {repository} from '@loopback/repository';
 import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
-} from '@loopback/repository';
-import {
+  HttpErrors,
+
   post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
+
+  requestBody
 } from '@loopback/rest';
-import {Registro} from '../models';
 import {RegistroRepository} from '../repositories';
+import {AuthService} from '../services/auth.service';
+
+
+
+
+class Credentials {
+  nombre_usuario: string;
+  contrasena: string;
+
+}
+
+
+
 
 export class RegistroController {
+
+  authService: AuthService;
+
   constructor(
     @repository(RegistroRepository)
-    public registroRepository : RegistroRepository,
-  ) {}
+    public registroRepository: RegistroRepository,
+  ) {
+    this.authService = new AuthService(this.registroRepository);
+  }
 
-  @post('/registro', {
+  @post('/login', {
     responses: {
       '200': {
-        description: 'Registro model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Registro)}},
-      },
-    },
+        description: "Login for Users"
+      }
+    }
   })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Registro, {
-            title: 'NewRegistro',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    registro: Omit<Registro, 'id'>,
-  ): Promise<Registro> {
-    return this.registroRepository.create(registro);
-  }
+  async login(
 
-  @get('/registro/count', {
-    responses: {
-      '200': {
-        description: 'Registro model count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async count(
-    @param.where(Registro) where?: Where<Registro>,
-  ): Promise<Count> {
-    return this.registroRepository.count(where);
-  }
+    @requestBody() credentials: Credentials)
 
-  @get('/registro', {
-    responses: {
-      '200': {
-        description: 'Array of Registro model instances',
-        content: {
-          'application/json': {
-            schema: {
-              type: 'array',
-              items: getModelSchemaRef(Registro, {includeRelations: true}),
-            },
-          },
-        },
-      },
-    },
-  })
-  async find(
-    @param.filter(Registro) filter?: Filter<Registro>,
-  ): Promise<Registro[]> {
-    return this.registroRepository.find(filter);
-  }
+    : Promise<object> {
+    let registro = await this.authService.Identify(credentials.nombre_usuario, credentials.contrasena);
+    if (registro) {
+      let tk = await this.authService.GenerateToken(registro);
+      return {
+        data: registro,
+        token: tk
+      }
+    } else {
+      throw new HttpErrors[401]("user or password invalida");
+    }
 
-  @patch('/registro', {
-    responses: {
-      '200': {
-        description: 'Registro PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Registro, {partial: true}),
-        },
-      },
-    })
-    registro: Registro,
-    @param.where(Registro) where?: Where<Registro>,
-  ): Promise<Count> {
-    return this.registroRepository.updateAll(registro, where);
-  }
 
-  @get('/registro/{id}', {
-    responses: {
-      '200': {
-        description: 'Registro model instance',
-        content: {
-          'application/json': {
-            schema: getModelSchemaRef(Registro, {includeRelations: true}),
-          },
-        },
-      },
-    },
-  })
-  async findById(
-    @param.path.string('id') id: string,
-    @param.filter(Registro, {exclude: 'where'}) filter?: FilterExcludingWhere<Registro>
-  ): Promise<Registro> {
-    return this.registroRepository.findById(id, filter);
-  }
 
-  @patch('/registro/{id}', {
-    responses: {
-      '204': {
-        description: 'Registro PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Registro, {partial: true}),
-        },
-      },
-    })
-    registro: Registro,
-  ): Promise<void> {
-    await this.registroRepository.updateById(id, registro);
-  }
-
-  @put('/registro/{id}', {
-    responses: {
-      '204': {
-        description: 'Registro PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() registro: Registro,
-  ): Promise<void> {
-    await this.registroRepository.replaceById(id, registro);
-  }
-
-  @del('/registro/{id}', {
-    responses: {
-      '204': {
-        description: 'Registro DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.registroRepository.deleteById(id);
   }
 }
---
+++ src/services/auth.service.ts
@@ -0,0 +1,52 @@
+import {repository} from '@loopback/repository';
+import {ServiceKeys as keys} from '../keys/service-keys';
+import {Registro} from '../models';
+import {RegistroRepository} from '../repositories';
+import {EncryptDecrypt} from './encrypt-decrypt.service';
+
+const jwt = require("jsonwebtoken");
+
+
+export class AuthService {
+  constructor(
+    @repository(RegistroRepository)
+    public registroRepository: RegistroRepository
+
+
+  ) {
+
+  }
+
+  async Identify(nombre_usuario: string, contrasena: string): Promise<Registro | false> {
+    console.log(`${nombre_usuario}  Password : ${contrasena}`);
+
+    let registro = await this.registroRepository.findOne({where: {nombre_usuario: nombre_usuario}});
+    if (registro) {
+      let cryptPass = new EncryptDecrypt(keys.LOGIN_CRYPT_METHOD).Encrypt(contrasena);
+      if (registro.contrasena == cryptPass) {
+        return registro;
+      }
+      console.log("ENTRA AQUI")
+
+    }
+    console.log("ERROR PERO DE QUE")
+    return false;
+  }
+
+  async GenerateToken(registro: Registro) {
+    registro.contrasena = '';
+    let token = jwt.sign({
+      exp: keys.TOKEN_EXPIRATION_TIME,
+      data: {
+        _id: registro.id,
+        nombre_usuario: registro.nombre_usuario,
+        rol: registro.rol,
+        usuarioId: registro.usuarioId
+      }
+    },
+      keys.JWT_SECRET_KEY);
+    return token;
+  }
+
+
+}
